9927e278ee9e0fb8663315f8dc0b07e6ad3fea07
Mathias Jose Miranda Rivera <mathiasmiranda110@gmail.com>
2022-05-10 03:08:46 -0500

    Actualización DB

--- db-scripts/02-create-products.sql
+++ db-scripts/02-create-products.sql
@@ -59,3 +59,86 @@ VALUES ('CAPTOPRIL-MED-1003', 'CAPTOPRIL', 'CAPTOPRIL, 25 mg','assets/images/pro
 
 INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
 VALUES ('CEFALEXINA-MED-1004', 'CEFALEXINA', 'CEFALEXINA, 250mg/5mL','assets/images/products/placeholder.png',1,100,24.99,1, NOW());
+
+##Jackeline
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('ASPIRINA-MED-1006', 'ASPIRINA', 'ASPIRINA, 500mg','assets/images/products/placeholder.png',1,100,26.99,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('OMEPRAZOL-MED-1007', 'OMEPPRAZOL', 'OMEPRAZOL, 40mg','assets/images/products/placeholder.png',1,100,10.99,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('LEXOTIROXINA-MED-1008', 'LEXOTIROXINA', 'LEXOTIROXINA, 40mg','assets/images/products/placeholder.png',1,100,12.99,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('RAMIPRIL-MED-1009', 'RAMIPRIL', 'RAMIPRIL, 5mg','assets/images/products/placeholder.png',1,100,31.99,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('AMLODIPINA-MED-1010', 'AMLODIPINA', 'AMLODIPINA, 5mg','assets/images/products/placeholder.png',1,100,10.99,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('PARACEMOL-MED-1011', 'PARACEMOL', 'PARACEMOL, 20mg','assets/images/products/placeholder.png',1,100,12.99,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('NITROGLICERINA-MED-1012', 'NITROGLICERINA', 'NITROGLICERINA, 50mg','assets/images/products/placeholder.png',1,100,12.99,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('SALBUTAMOL-MED-1013', 'SALBUTAMOL', 'SALBUTAMOL, 17mg','assets/images/products/placeholder.png',1,100,15.99,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('LANOPRAZOL-MED-1014', 'LANOPRAZOL', 'LANOPRAZOL, 30mg','assets/images/products/placeholder.png',1,100,21.99,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('CLEXANE-MED-1015', 'CLEXANE', 'CLEXANE, 40mg','assets/images/products/placeholder.png',1,100,12.99,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('CLEXANE-MED-1016', 'CLEXANE', 'CLEXANE, 40mg','assets/images/products/placeholder.png',1,100,13.99,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('DIGOXINA-MED-1017', 'DIGOXINA', 'DIGOXINA, 0.25mg','assets/images/products/placeholder.png',1,100,9.99,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('EPINEFRINA-MED-1018', 'EPINEFRINA', 'EPINEFRINA, 1mg','assets/images/products/placeholder.png',1,100,20.99,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('FUROSEMIDA-MED-1019', 'FUROSEMIDA', 'FUROSEMIDA, 20mg','assets/images/products/placeholder.png',1,100,14.99,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('ESPIRONOLACTONA-MED-1020', 'ESPIRONOLACTONA', 'ESPIRONOLACTONA, 25mg','assets/images/products/placeholder.png',1,100,10.99,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('POLIDOCANOL-MED-1021', 'POLIDOCANOL', 'POLIDOCANOL, 40mg','assets/images/products/placeholder.png',1,100,10.99,1, NOW()); 
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('NIFEDIPINO-MED-1022', 'NIFEDIPINO', 'POLIDOCANOL, 30mg','assets/images/products/placeholder.png',1,100,12.99,1, NOW());
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('PREDNISONA-MED-1023', 'PREDNISONA', 'PREDNISONA, 5mg','assets/images/products/placeholder.png',1,100,13.99,1, NOW()); 
+
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('ANFENICOL-MED-1024', 'ANFENICOL', 'ANFENICOL, 40mg','assets/images/products/placeholder.png',1,100,15.99,1, NOW());
+#Mathias
+INSERT INTO product_category(category_name) VALUES ('SUPLEMENTOS');
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('PEDIASURE-SUP-2001', 'PEDIASURE', 'PEDIASURE, 400g','assets/images/products/pediasure.png',1,200,41.99,2, NOW());
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('ENSURE-SUP-2002', 'ENSURE', 'ENSURE, 850g','assets/images/products/ensure.png',1,200,85.99,2, NOW());
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('PROVIDE-SUP-2003', 'PROVIDE GOLD', 'PROVIDE GOLD, 887 ml','assets/images/products/provide.png',1,200,90.00,2, NOW());
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('CALCIFOR-SUP-2004', 'CALCIFOR', 'CALCIFOR, 60 capsulas','assets/images/products/calcifor.png',1,200,51.99,2, NOW());
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('GERIAMEL-SUP-2005', 'GERIAMEL', 'GERIAMEL Vainilla, 1000 g','assets/images/products/geriamel.png',1,200,94.90,2, NOW());
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('WHEYPROTEIN-SUP-2006', 'WHEY PROTEIN', 'WHEY PROTEIN, SABOR CHOCOLATE','assets/images/products/whey.png',1,200,269.00,2, NOW());
+INSERT INTO product_category(category_name) VALUES ('CUIDADO PERSONAL');
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('HOJASGILETTE-CUI-3001', 'HOJAS GILETTE PRESTOBARBA 3', 'HOJAS GILETTE PRESTOBARBA 3, 2 hojas ','assets/images/products/hojas.png',1,100,9.90,3, NOW());
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('ESPUMALEA-CUI-3002', 'ESPUMA DE AFEITAR LEA', 'ESPUMA DE AFEITAR LEA, 250 ml ','assets/images/products/espumalea.png',1,200,16.00,3, NOW());
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('ESPUMAGILETTE-CUI-3003', 'ESPUMA DE AFEITAR GILETTE', 'ESPUMA DE AFEITAR GILETTE, 250 ml','assets/images/products/espumagilette.png',1,200,31.90,3, NOW());
+INSERT INTO product (sku, name, description, image_url, active, units_in_stock,unit_price, category_id, date_created)
+VALUES ('GELAFEITAR-CUI-3004', 'GEL DE AFEITAR LEA', 'GEL DE AFEITAR LEA, 200 ml','assets/images/products/gellea.png',1,200,23.90,3, NOW());
+
+
